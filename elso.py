7ea15740ce27c3698cd71d4f33c64e5216762b51
Your Name <you@example.com>
2022-10-24 17:06:35 +0200

    egy

---
+++ elso.py
@@ -0,0 +1,35 @@
+print("Helló üdv itt!")
+print(23*12)
+print("23*12 =",23*12)
+#print("23*12 ="+23*12) #hibás
+#print("23*12 ="+(23*12) ) hibás
+print("Alma"+" "+"körte")
+print("Alma","körte")
+print("Alma","körte",sep=", ")
+print("Alma","körte","barack",sep="\t")
+print("Alma","körte","barack",sep="\n")
+
+print("Alma","körte","barack",sep="\a") # azonos "" -el
+print("Alma","körte","barack",sep="\b") #  backspace
+print("Alma","körte","barack",sep="\b\b\b") # 
+print("Alma","körte","barack",sep="\f") # sor le - formfeed
+print("Alma","körte","barack",sep="\n") # új sor
+print("Alma","körte","barack",sep="\r") # sor elejére
+print("Alma","körte","barack",sep="\t") # tab
+print("Alma","körte","barack",sep="\xab")
+print("Alma","körte","barack",sep="\x41")
+print("Alma","körte","barack",sep="\v") # sor le
+print("Alma","körte","barack",sep="\033") # 
+print("Alma","körte","barack",sep="\x07") # 
+print("Szia \\ pipacs")
+print("Szia \' pipacs")
+print("Szia ' pipacs")
+print("Szia \"pipacs\"")
+#print("Szia "pipacs")
+print('Szia',end="")
+print('pipacs')
+
+print('Szia',end=" ")
+print('pipacs')
+print("alma")
+
